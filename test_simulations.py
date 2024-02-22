a35f52f3e808c51aa7ab7743b4e81225499fd9b7
Gauthier <42948474+GauthierBeaudoux@users.noreply.github.com>
2024-02-22 11:55:15 +0100

    up quasi random

--- test_simulations.py
+++ test_simulations.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from scipy.stats import qmc
+import scipy.stats as stats
 
 
 
@@ -14,29 +16,37 @@ def delta_t(n, T):
     return t, dt
 
 
-def mouvement_brownien(n, T, x0, r, sigma):
+def mouvement_brownien(n, T, x0, r, sigma, methode="Pseudo"):
     """
     Renvoie une liste contenant les valeurs du mouvement brownien du temps 0 à T
     """
     t, dt = delta_t(n, T)
     B = np.zeros(n)
     B[0] = x0
+    if methode == "Quasi":
+        distrib = halton_norm(n-1)
     for i in range(1, n):
-        B[i] = B[i-1] + np.random.normal(loc=r*dt, scale=sigma*np.sqrt(dt))
+        if methode == "Pseudo":
+            # B[i] = B[i-1] + np.random.normal(loc=r*dt, scale=sigma*np.sqrt(dt))
+            B[i] = B[i-1] + (r*dt + sigma*np.sqrt(dt)*np.random.normal(loc=0, scale=1))
+        elif methode == "Quasi":
+            print(float(distrib[i-1]))
+            B[i] = B[i-1] + (r*dt + sigma*np.sqrt(dt)*float(distrib[i-1]))
+        else:
+            raise Exception("Mauvaise méthode !")
     return B
 
-def mouvement_brownien_standard(n, T):
-    return mouvement_brownien(n, T, 0, 0, 1)
+def mouvement_brownien_standard(n, T, methode="Pseudo"):
+    return mouvement_brownien(n, T, 0, 0, 1, methode)
 
 
-
-def black_scholes(S0, n, T, r, d, sigma, renvoie_brownien=False):
+def black_scholes(S0, n, T, r, d, sigma, methode="Pseudo", renvoie_brownien=False):
     """
     Renvoie une liste contenant l'évolution du prix d'un actif sous-jacent suivant le modèle de Black-Scholes
     Peut aussi renvoyer le mouvement brownien correspondant
     """
     temps, dt = delta_t(n, T)
-    W = mouvement_brownien_standard(n, T)
+    W = mouvement_brownien_standard(n, T, methode)
     S = []
     for t, Wt in zip(temps, W):
         S.append(S0*np.exp((r-d)*t - sigma**2*t/2 + sigma*Wt))
@@ -78,18 +88,36 @@ def payoff(S, K, type_payoff, T=1, frequence_barriere="M", valeur_barriere=80):
         case _:
             raise ValueError("Mauvais type de payoff")
 
-def prix_instant_initial(S0, n, T, r, d, sigma, nb_simulations, K, type_payoff, frequence_mesure=1000,frequence_barriere="M", valeur_barriere=80):
+def prix_instant_initial(S0, n, T, r, d, sigma, nb_simulations, K, type_payoff, frequence_mesure=1000,frequence_barriere="M", valeur_barriere=80, methode="Pseudo"):
     H = 0
     prix = []
     iterations = []
     for i in tqdm(range(1,nb_simulations+1)):
-        S = black_scholes(S0, n, T, r, d, sigma)
+        S = black_scholes(S0, n, T, r, d, sigma, methode)
         H += payoff(S,K,type_payoff,T=T, frequence_barriere=frequence_barriere, valeur_barriere=valeur_barriere)
         if i % frequence_mesure == 0:
             esperance_payoff = H/i
             prix.append(np.exp(-r*T)*esperance_payoff)
             iterations.append(i)
     return prix, iterations
+
+def halton(n, d=1):
+    """
+    Fonction uniforme entre 0 et 1
+    """
+    sampler = qmc.Halton(d, scramble=True)
+    return sampler.random(n)
+
+def halton_norm(n, d=1):
+    sampler = qmc.Halton(d, scramble=True)
+    x_halton = sampler.random(n)
+    return stats.norm.ppf(x_halton)
+
+# B = mouvement_brownien_standard(1000,1, methode="Quasi")
+# plt.plot(B)
+# plt.show()
+# exit()
+
 
 S0 = 100
 sigma = 0.1
@@ -98,21 +126,38 @@ d = 0.03
 K = 103
 T = 1
 n = 12
-nb_simulations = 10**4
+nb_simulations = 10**5
 H = 80
 frequence_barriere = "M"
 
+def autre_simul():
+    Z = funcs[method](M)
+    delta_St = nudt + volsdt*Z
+    ST = S0*np.exp(delta_St)
+    CT = np.maximum(0, ST - K)
+    C0 = np.exp(-r*T)*np.sum(CT)/M
+    prix = []
+    iterations = []
+    for i in tqdm(range(1,nb_simulations+1)):
+        if i % frequence_mesure == 0:
+            esperance_payoff = H/i
+            prix.append(np.exp(-r*T)*esperance_payoff)
+            iterations.append(i)
+    return prix, iterations
 
-result, frequence  = prix_instant_initial(S0, n, T, r, d, sigma,
-                    nb_simulations, K, "Call EU",
-                    frequence_mesure=50,
-                    frequence_barriere=frequence_barriere,
-                    valeur_barriere=H)
+
+result, iterations  = prix_instant_initial(S0, n, T, r, d, sigma,
+                                           nb_simulations, K, "Call EU",
+                                           frequence_mesure=50,
+                                           frequence_barriere=frequence_barriere,
+                                           valeur_barriere=H,
+                                           methode="Pseudo")
 
 print(result[-1])
 # 1.2564532648137718 avec 1 million de simulations, Call Asiatique
 # 3.441207795600795 avec 1 million de simu, Call EU
 # 3.4409126762138484 avec 1 million de simu, Call down and out
-plt.plot(frequence, result)
+plt.plot(iterations, result)
+plt.axhline(y=3.441207795600795, color='r')
 plt.savefig("Call_EU.png")
 plt.show()
